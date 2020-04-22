7157301d342eb8ba5e5b1b0fc308811fa40fce17
gyeongmo_806 <jip080620@gmail.com>
2020-04-23 02:44:21 +0900

    read selected id content

--- app.js
+++ app.js
@@ -2,20 +2,34 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 const port = 3000
-
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
     host:"localhost",
     user: "root",
-    password: "1q2w3e4r"
+    password: "1q2w3e4r",
+    database: "my_db"
 })
 
-connection.connect( (err) => {
+//mysql connect
+con.connect( (err) => {
     if(err) console.log(err)
     console.log("Mysql Connect!")
 })
-
-app.get('/', (req,res) => {
-    res.send("Hello")
+//load whole contents
+app.get('/api/loadcontents', (req,res) => {
+    const sql = 'SELECT * FROM CONTENTS'
+    con.query(sql, (err,rows,fields) => {
+        if(err) console.log(err)
+        console.log(rows)
+        res.send(rows)
+    })
+})
+//read only content what collect to id 
+app.get('/api/read/:id', (req,res) => {
+    const sql = `SELECT * FROM CONTENTS WHERE id=${req.params.id}`
+    con.query(sql, (err, rows, fields) => {
+        if(err) console.log(err)
+        res.send(rows)
+    })
 })
 
 app.listen(port, () => {
