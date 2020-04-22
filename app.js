76c5b2e1db42e6a3e626b7f679130fbebc196f48
gyeongmo_806 <jip080620@gmail.com>
2020-04-23 02:21:20 +0900

    add mysql module

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const port = 3000
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "1q2w3e4r"
+})
+
+connection.connect( (err) => {
+    if(err) console.log(err)
+    console.log("Mysql Connect!")
+})
+
+
+app.get('/', (req,res) => {
+    res.send("Hello")
+})
+
+app.listen(port, () => {
+    console.log(`Now listen on port: ${port}`)
+})
